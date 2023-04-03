a99387913c304ec6481b76c9a5114ce456aeff0a
SE13 <sebastien@attestis.com>
2023-04-03 14:40:51 +0200

    implemented controller Category for meeting the book search and consultation goal

--- src/main/java/com/l2i_e_commerce/controller/CategoryController.java
+++ src/main/java/com/l2i_e_commerce/controller/CategoryController.java
@@ -1,9 +1,11 @@
 package com.l2i_e_commerce.controller;
 
+import com.l2i_e_commerce.model.Category;
 import com.l2i_e_commerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -11,5 +13,30 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable Long id) {
+        return categoryService.findById(id);
+    }
+
+    @GetMapping
+    public List<Category> getCategories() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.save(category);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryService.update(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteById(id);
+    }
 }
 
