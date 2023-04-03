b2b21f2b80bc141078cd08e1013d5eaa86fc7db7
SE13 <sebastien@attestis.com>
2023-04-03 14:54:05 +0200

    implemented controller Book for meeting the book search and consultation goal

--- src/main/java/com/l2i_e_commerce/controller/BookController.java
+++ src/main/java/com/l2i_e_commerce/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.l2i_e_commerce.controller;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.l2i_e_commerce.model.Book;
 import com.l2i_e_commerce.service.BookService;
 
 @RestController
@@ -14,6 +17,37 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return ResponseEntity.ok(bookService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Book book = bookService.findById(id);
+        if (book != null) {
+            return ResponseEntity.ok(book);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.save(book));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        book.setId(id);
+        return ResponseEntity.ok(bookService.update(book));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
 
